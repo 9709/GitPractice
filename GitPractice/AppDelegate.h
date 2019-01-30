b3779e11eab00b30281ce95491024bad87baeeda
Matthew Chan <mat_t4@hotmail.com>
2019-01-29 21:39:59 -0800

    Created

---
+++ GitPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitPractice
+//
+//  Created by Matthew Chan on 2019-01-29.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
